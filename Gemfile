07f59e3d0542dea1a9a1065d4d808926a324e878
John Allen <john@threedogconsulting.com>
2017-03-19 14:49:42 -0400

    Add support for Github "review requests"
    * Adds a config setting to enable review requests (defaults to false)
    * Adds a config setting to enable comments (defaults to true)
    * Allows for both a traditional comment and review request
    * Introduces a class to encapsulate interactions with Github
    * Refactors #review_on_github exceptions are handles as such

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
 gemspec
+
+# TODO: remove me after next release of the `octokit` gem
+gem "octokit", "github" => "octokit/octokit.rb"
--- lib/lita-reviewme.rb
+++ lib/lita-reviewme.rb
@@ -4,4 +4,14 @@ Lita.load_locales Dir[File.expand_path(
   File.join("..", "..", "locales", "*.yml"), __FILE__
 )]
 
+require "lita-reviewme/github"
+
 require "lita/handlers/reviewme"
+
+module Lita
+  module Reviewme
+    class NoReviewer < StandardError; end
+    class UnknownOwner < StandardError; end
+    class CannotPostComment < StandardError; end
+  end
+end
